12213b6c021572239b1c9da2e62fa34558305cf4
Oleg Sampara <lazers03@mail.ru>
2022-10-11 11:02:45 +0300

    Edit text added

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -10,12 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //funny
+        var answ=0.01
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             val toast = Toast.makeText(this, "Completed!", Toast.LENGTH_SHORT)
             toast.show()
-            val resultTextView: TextView = findViewById(R.id.textView6)
-            resultTextView.text = "6"
+            val final: TextView = findViewById(R.id.textView6)
+            val a: TextView = findViewById(R.id.editText); val a1=Integer.parseInt( a.getText().toString());
+            val b: TextView = findViewById(R.id.editText1)
+            val c: TextView = findViewById(R.id.editText2)
+            val d: TextView = findViewById(R.id.editText3)
+
+            final.text = "$a1"
 
 
         }
