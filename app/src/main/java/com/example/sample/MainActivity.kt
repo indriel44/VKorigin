4fa1bc215409ae36b48c7e8b27b4175eb93f7cbd
Oleg Sampara <lazers03@mail.ru>
2022-10-11 09:11:29 +0300

    UI added

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //funny
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Completed!", Toast.LENGTH_SHORT)
+            toast.show()
+            val resultTextView: TextView = findViewById(R.id.textView6)
+            resultTextView.text = "6"
+
+
+        }
+
+
     }
 }
